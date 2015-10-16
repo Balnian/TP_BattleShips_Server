fa3a57f247fea7b137754f88da919cd010adf3fc
Francis <super_f_qc@hotmail.com>
2015-10-16 12:57:05 -0400

    Log Console

---
+++ TP_BattleShips_Server/LogConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_BattleShips_Server
+{
+    class LogConsole
+    {
+        public static void Log(String Message)
+        {
+            Console.WriteLine(">> " + Message);
+        }
+        public static void LogWithTime(String Message)
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ">> " + Message);
+        }
+    }
+}
